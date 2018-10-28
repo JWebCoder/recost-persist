f028c05699adac9b729eff355e62cbef2e16630e
João Moura <joao.moura@b-i.com>
2018-10-28 18:37:39 +0000

    First commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function persister (target = 'recost-persister') {
+  return {
+    after: (state) => {
+      window.localStorage.setItem(target, JSON.stringify(state))
+    },
+  }
+}
+
+function reloadPersistedState (target = 'recost-persister', initial = {}) {
+  return {
+    ...initial,
+    ...(JSON.parse(window.localStorage.getItem(target)) || {}),
+  }
+}
+
+export {
+  reloadPersistedState,
+  persister,
+}
